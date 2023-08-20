7d785a2f9d3c5f4d2c56a1087443d3bec8647532
Chanfi Attoumanni <attoumani.chanfi@gmail.com>
2023-08-20 07:48:41 +0200

    exercice 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function GuessInput() {
+  const [guessInput, setGuessInput] = React.useState("");
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    // Prevent empty string
+    if(guessInput.length === 0) return;
+
+    console.log({guess: guessInput});
+    setGuessInput("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={formSubmitHandler}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input 
+        id="guess-input" 
+        type="text" 
+        pattern="[a-zA-Z]{5,5}"
+        value={guessInput}
+        onChange={event => {
+          // Prevent answer with more than 5 letters
+          if(event.target.value.length > 5) return;
+
+          const nextGuessInput = event.target.value.toUpperCase();
+          setGuessInput(nextGuessInput);
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
